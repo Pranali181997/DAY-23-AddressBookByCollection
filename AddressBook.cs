ca3f1f148dd83e61767057fcc0d058be4e769891
Pranali <pranalilambat00@gmail.com>
2022-03-10 06:06:02 +0530

    UC-2_AddContact

---
+++ AddressBook.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AddressBook_Collections
+{
+    class AddressBook
+    {
+        //creation of list
+        public static List<Contact> People;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AddressBook"/> class.
+        /// </summary>
+        public AddressBook()
+        {
+            People = new List<Contact>();
+        }
+
+        /// <summary>
+        /// Finds the contact.
+        /// </summary>
+        /// <param name="fname">The fname.</param>
+        /// <returns></returns>
+        public Contact FindContact(string fname)
+        {
+            Contact contact = People.Find((person) => person.FirstName == fname);
+            return contact;
+        }
+
+        /// <summary>
+        /// Adds the contact.
+        /// </summary>
+        /// <param name="FirstName">The first name.</param>
+        /// <param name="LastName">The last name.</param>
+        /// <param name="Address">The address.</param>
+        /// <param name="City">The city.</param>
+        /// <param name="State">The state.</param>
+        /// <param name="ZipCode">The zip code.</param>
+        /// <param name="PhoneNumber">The phone number.</param>
+        /// <param name="Email">The email.</param>
+        /// <returns></returns>
+        public bool AddContact(string FirstName, string LastName, string Address, string City, string State, string ZipCode, string PhoneNumber, string Email)
+        {
+            Contact contact = new Contact(FirstName, LastName, Address, City, State, ZipCode, PhoneNumber, Email);
+            Contact result = FindContact(FirstName);
+            if (result == null)
+            {
+                People.Add(contact);
+                return true;
+            }
+            else
+                return false;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,11 +4,64 @@ using System.Collections.Generic;
 namespace AddressBook_Collections
 {
     class Program
-    {       
+    {
+        //declaration
+        AddressBook book;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Program"/> class.
+        /// </summary>
+        public Program()
+        {
+            book = new AddressBook();
+        }
+
+        /// <summary>
+        /// Defines the entry point of the application.
+        /// </summary>
+        /// <param name="args">The arguments.</param>
         static void Main(string[] args)
         {
+            //creation of object
+            Program p = new Program();
+            //welcome message
             Console.WriteLine("Hello, Welcome to Address Book!");
-                     
+            //main menu
+            Console.WriteLine("*****Main Menu*****");
+            Console.WriteLine("Select the option. \n1. Add new contact. \n2. Edit existing contact. \n3. Exit.");
+            int option = int.Parse(Console.ReadLine());
+            switch (option)
+            {
+                case 1:
+                    Console.WriteLine("Enter the person details to be added in the address book");
+                    Console.WriteLine("First Name");
+                    string FirstName = Console.ReadLine();
+                    Console.WriteLine("Last Name");
+                    string LastName = Console.ReadLine();
+                    Console.WriteLine("Address");
+                    string Address = Console.ReadLine();
+                    Console.WriteLine("City");
+                    string City = Console.ReadLine();
+                    Console.WriteLine("State");
+                    string State = Console.ReadLine();
+                    Console.WriteLine("Zip code");
+                    string ZipCode = Console.ReadLine();
+                    Console.WriteLine("Phone Number");
+                    string PhoneNumber = Console.ReadLine();
+                    Console.WriteLine("Email");
+                    string Email = Console.ReadLine();
+                    //calling AddContact function
+                    if (p.book.AddContact(FirstName, LastName, Address, City, State, ZipCode, PhoneNumber, Email))
+                    {
+                        Console.WriteLine("Contact added successfully");
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Contact already exists");
+                        break;
+                    }
+            }
         }
     }
 }
